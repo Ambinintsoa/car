55fc710b7c7eefacf022e0f1a2d9f99fbec71f4a
Jaonaaa <jaonapaulferdinah@gmail.com>
2024-01-24 19:43:23 +0300

    debugging 307

--- src/main/java/com/commercial/commerce/chat/controller/WSController.java
+++ src/main/java/com/commercial/commerce/chat/controller/WSController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class WSController {
@@ -27,4 +29,11 @@ public class WSController {
         System.out.println(message.toString());
         service.notifyUser(message.getReceiver_id(), message);
     }
+
+    @GetMapping("/msg")
+    public Object message() {
+        System.out.println(" Ato message");
+        return new Message();
+    }
+
 }
